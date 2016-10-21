afc43c95c6c847d049ab06e4af800db0640c9c72
kvasenko <kvasenko@ukr.net>
2016-10-21 15:38:57 +0300

    Added package auth to composer.json

--- src/controllers/widgets/auth.php
+++ src/controllers/widgets/auth.php
@@ -20,7 +20,7 @@ return function () {
      * @var Controller $this
      */
     ?>
-    <a href="<?=Router::getUrl('auth', 'auth', ['provider'=> 'google'])?>"
-       class="provider btn fa fa-google-plus-square fa-2x"></a>
+    <a href="<?=Router::getUrl('auth', 'auth', ['provider'=> 'twitter'])?>"
+       class="provider btn fa fa-twitter-square fa-2x"></a>
     <?php
 };
